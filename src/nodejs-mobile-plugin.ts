7b33dcdfe304ec85c08e4d36899b708661e7e3fb
Lokke <accounts.lokke@googlemail.com>
2025-10-25 12:14:11 +0200

    Fix: Remove nodejs-mobile-plugin.ts and ignore dist-electron build artifacts

--- src/nodejs-mobile-plugin.ts
+++
@@ -1,9 +0,0 @@
-import { registerPlugin } from '@capacitor/core';
-
-// This plugin enables Node.js backend on Android
-const NodeJSMobile = registerPlugin('NodeJSMobile', {
-  web: () => import('./web').then(m => new m.NodeJSMobileWeb()),
-});
-
-export * from './definitions';
-export { NodeJSMobile };
